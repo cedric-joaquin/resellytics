d2a50485cc63f924e95b3a021dc5c390567bb398
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-08 22:50:23 -0700

    enabled sessions in app controller instead of config.ru

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "JmPCuw6s"
   end
 
   get '/' do
@@ -12,8 +14,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/dashboard' do
-
-    erb :dashboard
+    if logged_in?
+      erb :dashboard
+    else
+      redirect '/login'
+    end
   end
 
   helpers do
@@ -24,5 +29,6 @@ class ApplicationController < Sinatra::Base
     def current_user
       User.find(session[:user_id])
     end
+  end
 
 end
