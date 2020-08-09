c023e004a5ae59ac60a7b683a840234d5da34045
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-09 11:11:19 -0700

    updated seed to create two users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
 require 'faker'
 
-user = User.create(
+user1 = User.create(
     username: "Test",
     email: "test@email.com",
+    password: "123qwe"
+)
+
+user2 = User.create(
+    username: "2nd User"
+    email: "2nd@email.com"
     password: "123qwe"
 )
 
@@ -16,5 +22,18 @@ user = User.create(
         item_cost: rand(1.0..100.0).round(2),
         purchase_date: date.strftime('%B %d, %Y')
     )
-    user.items << item
+    user1.items << item
+end
+
+5.times do
+    date = Faker::Date.in_date_period
+    item = Item.create(
+        name: Faker::Commerce.product_name,
+        brand: Faker::Company.name,
+        size: rand(7..12).to_s,
+        purchased_from: Faker::Address.city,
+        item_cost: rand(1.0..100.0).round(2),
+        purchase_date: date.strftime('%B %d, %Y')
+    )
+    user2.items << item
 end
