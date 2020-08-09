e23b601358d6ba4a499179158057a4bb9fae8dda
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-08 22:34:36 -0700

    added user session when logged in

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -8,6 +8,7 @@ class UserController < ApplicationController
         user = User.find_by(username: params[:username])
 
         if user && user.authenticate(params[:password])
+            session[:user_id] = user.id
             redirect '/dashboard'
         else
             redirect '/login'
