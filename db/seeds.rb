e77f70bff1c974b1815c323c4fd0b62edbb4fee8
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-09 01:18:16 -0700

    updated seed to include random purchase date

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,14 @@ user = User.create(
 )
 
 5.times do
+    date = Faker::Date.in_date_period
     item = Item.create(
         name: Faker::Commerce.product_name,
         brand: Faker::Company.name,
         size: rand(7..12).to_s,
         purchased_from: Faker::Address.city,
-        item_cost: rand(1.0..100.0).round(2)
+        item_cost: rand(1.0..100.0).round(2),
+        purchase_date: date.strftime('%B %d, %Y')
     )
     user.items << item
 end
