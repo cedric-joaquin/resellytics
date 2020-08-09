5455dea17fb92ad7dacb60d44153364be3132851
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-09 00:37:59 -0700

    added seed file and created user and items

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+require 'faker'
+
+user = User.create(
+    username: "Test",
+    email: "test@email.com",
+    password: "123qwe"
+)
+
+5.times do
+    item = Item.create(
+        name: Faker::Commerce.product_name,
+        brand: Faker::Company.name,
+        size: rand(7..12).to_s,
+        purchased_from: Faker::Address.city,
+        item_cost: rand(1.0..100.0)
+    )
+    user.items << item
+end
