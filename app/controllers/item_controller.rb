594e7a6c5d10def29a91ea14265fbe8a673b8e6c
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-09 00:44:25 -0700

    updated to retrieve only items belonging to user

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -2,7 +2,7 @@ class ItemController < ApplicationController
 
     get '/items' do
         redirect '/' if !logged_in?
-        @items = User.items
+        @items = User.find(session[:user_id]).items
 
         erb :'/items/index'
     end
