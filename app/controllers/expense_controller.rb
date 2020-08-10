55ca245f10782dcdd46c21ddebb7c06e06a8b44b
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-09 20:57:45 -0700

    copied item_controller as template for expense controller

---
+++ app/controllers/expense_controller.rb
@@ -0,0 +1,62 @@
+class ExpenseController < ApplicationController
+
+    get '/items' do
+        redirect '/' if !logged_in?
+        @items = User.find(session[:user_id]).items
+
+        erb :'/items/index'
+    end
+    
+    get '/items/new' do
+        redirect '/' if !logged_in?
+        erb :'/items/create'
+    end
+
+    get '/items/:id' do
+        redirect '/' if !logged_in?
+
+        @item = Item.find(params[:id])
+        #Redirects back to inventory page if item from URL does not belong to logged in user
+        redirect '/items' if !User.find(session[:user_id]).items.include?(@item)
+
+        erb :'/items/show'
+    end
+
+    get '/items/:id/edit' do
+        @item = Item.find(params[:id])
+        redirect '/items' if !User.find(session[:user_id]).items.include?(@item)
+        
+        erb :'/items/update'
+    end
+
+    patch '/items/:id' do
+        item = Item.find(params[:id])
+        item.update(
+            name: params[:name],
+            brand: params[:brand],
+            size: params[:size],
+            item_cost: params[:item_cost],
+            purchased_from: params[:purchased_from],
+            purchase_date: params[:purchase_date]
+        )
+
+        redirect "/items/#{item.id}"
+    end
+
+    post '/items' do
+        date = DateTime.parse(params[:purchase_date]).to_date
+        params[:purchase_date] = date.strftime('%B %d, %Y')
+        item = Item.create(params)
+        User.find(session[:user_id]).items << item
+        redirect "/items/#{item.id}"
+    end
+
+    delete '/items/:id' do
+        Item.find(params[:id]).destroy
+
+        redirect '/items'
+    end
+
+
+
+end
