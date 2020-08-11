68d4c0fec8a5cebd00c7d4984b273c035ade5cc1
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-10 20:05:02 -0700

    changed fake data to be more "relevant"

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 require 'faker'
+BRANDS = ["Adidas","Nike","Supreme"]
+STORES = ["Kith","Dover Street Market","Shop Nice Kicks", "Supreme New York", "Footlocker", "Finishline"]
+CATEGORIES = ["Bots", "Proxies", "Consumables", "Accounts", "Miscellaneous"]
+
 
 user1 = User.create(
     username: "Test",
@@ -16,9 +20,9 @@ user2 = User.create(
     date = Faker::Date.in_date_period
     item = Item.create(
         name: Faker::Commerce.product_name,
-        brand: Faker::Company.name,
+        brand: BRANDS[rand(0..BRANDS.size - 1)],
         size: rand(7..12).to_s,
-        purchased_from: Faker::Address.city,
+        purchased_from: STORES[rand(0..STORES.size-1)],
         cost: rand(1.0..100.0).round(2),
         purchase_date: date.strftime('%B %d, %Y')
     )
@@ -29,9 +33,9 @@ end
     date = Faker::Date.in_date_period
     item = Item.create(
         name: Faker::Commerce.product_name,
-        brand: Faker::Company.name,
+        brand: BRANDS[rand(0..BRANDS.size - 1)],
         size: rand(7..12).to_s,
-        purchased_from: Faker::Address.city,
+        purchased_from: STORES[rand(0..STORES.size-1)],
         cost: rand(1.0..100.0).round(2),
         purchase_date: date.strftime('%B %d, %Y')
     )
@@ -42,8 +46,8 @@ end
     date = Faker::Date.in_date_period
     expense = Expense.create(
         name: Faker::Commerce.product_name,
-        category: Faker::Company.name,
-        purchased_from: Faker::Address.city,
+        category: CATEGORIES[rand(0..CATEGORIES.size-1)],
+        purchased_from: STORES[rand(0..STORES.size-1)],
         cost: rand(1.0..100.0).round(2),
         purchase_date: date.strftime('%B %d, %Y')
     )
@@ -54,8 +58,8 @@ end
     date = Faker::Date.in_date_period
     expense = Expense.create(
         name: Faker::Commerce.product_name,
-        category: Faker::Company.name,
-        purchased_from: Faker::Address.city,
+        category: CATEGORIES[rand(0..CATEGORIES.size-1)],
+        purchased_from: STORES[rand(0..STORES.size-1)],
         cost: rand(1.0..100.0).round(2),
         purchase_date: date.strftime('%B %d, %Y')
     )
