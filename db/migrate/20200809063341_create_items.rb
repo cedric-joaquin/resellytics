7fd4df0b90c3d9ba75ecf5b66271673891847183
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-10 17:50:36 -0700

    updated column name for item cost

--- db/migrate/20200809063341_create_items.rb
+++ db/migrate/20200809063341_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string :brand
       t.string :size
       t.string :purchased_from
-      t.float :item_cost
+      t.float :cost
       t.integer :user_id
       t.timestamps
     end
