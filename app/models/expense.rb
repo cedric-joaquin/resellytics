09a9ffad6464dab8cfa143b89118f3848f3148d9
cedric-joaquin <joaquin.cedric@gmail.com>
2020-08-19 18:03:36 -0700

    removed class method total from models

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,9 +1,3 @@
 class Expense < ActiveRecord::Base
     belongs_to :user
-
-    def self.total(user_id)
-        Expense.all.select{|e|e.user_id == user_id}.collect do |expense|
-            expense.cost
-        end.inject(0, :+)
-    end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,3 @@
 class Item < ActiveRecord::Base
     belongs_to :user
-
-    def self.total(user_id)
-        Item.all.select{|i|i.user_id == user_id}.collect do |item|
-            item.cost
-        end.inject(0, :+)
-    end
 end
